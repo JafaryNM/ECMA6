d77293e77c121daaab857a07af567351ea34af5d
JafaryNm <jaffarymdegela@gmail.com>
2022-10-02 20:38:44 +0300

    JavaScript class constructor updated

---
+++ scripts12.js
@@ -0,0 +1,24 @@
+class Car{
+   constructor({ title}){
+
+    thies.title=title
+
+    }
+    drive(){
+        return "Voom is very fantastic"
+    }
+
+}
+
+const car= new Car({title:'Toyota'})
+console.log(car.drive())
+console.log(car.title)
+
+
+
+
+
+
+
+
+
